a15ec0bcf46f84aa112c9ef34d8620549837bc83
DeaconLi <446995198@qq.com>
2018-08-19 21:12:12 +0800

    inti

---
+++ spring-cloud-lesson-3-config-client/src/main/java/com/segmentfault/springcloudlesson3configclient/SpringCloudLesson3ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.segmentfault.springcloudlesson3configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLesson3ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLesson3ConfigClientApplication.class, args);
+	}
+}
