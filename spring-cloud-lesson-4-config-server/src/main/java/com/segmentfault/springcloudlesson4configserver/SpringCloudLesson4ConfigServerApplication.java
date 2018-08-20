8ed5d0c85b1a00f1aadf866a191f70a3eefeab8c
DeaconLi <446995198@qq.com>
2018-08-20 23:44:13 +0800

    Eureka Server、Eureka Client、Config Server、Config Server Integration

---
+++ spring-cloud-lesson-4-config-server/src/main/java/com/segmentfault/springcloudlesson4configserver/SpringCloudLesson4ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.segmentfault.springcloudlesson4configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+//@EnableDiscoveryClient
+public class SpringCloudLesson4ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLesson4ConfigServerApplication.class, args);
+	}
+}
